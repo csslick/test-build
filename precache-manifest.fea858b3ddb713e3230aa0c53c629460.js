16424adb6b557c74d9fc7c882eff6d10fa3ef352
csslick <digitone@naver.com>
2020-06-20 22:03:48 +0900

    Add files via upload

---
+++ precache-manifest.fea858b3ddb713e3230aa0c53c629460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352d0fbc62a00d392f72996c27628f9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d84999d8943d5f257a6",
+    "url": "/static/css/main.79826236.chunk.css"
+  },
+  {
+    "revision": "54323b2632fc2549e37c",
+    "url": "/static/js/2.824ff911.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.824ff911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d84999d8943d5f257a6",
+    "url": "/static/js/main.c7ba0140.chunk.js"
+  },
+  {
+    "revision": "7c0b3cb883cfae46baa6",
+    "url": "/static/js/runtime-main.cdeaf004.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-build/precache-manifest.1ff67b59e1ba5ab844bf9695e61076e5.js"
+  "/precache-manifest.fea858b3ddb713e3230aa0c53c629460.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
